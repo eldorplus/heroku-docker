54318e22558fea71a4658adfe1c81ed04b013cb7
Matthew Duffy <mattduffy@gmail.com>
2017-06-03 16:01:02 -0700

    Set local app config settings.

---
+++ webapp/config/config.js
@@ -0,0 +1,13 @@
+'use strict';
+//module.exports = require('./' + (process.env.NODE_ENV || 'development') + '.json');
+module.exports = {
+	"appName": process.env.APP_NAME,
+	"appVersion": process.env.APP_VERSION,
+	"dbURL": "mongodb://mongodb:27017/node_core",
+	"sessionSecret": process.env.SESSION_SECRET,
+	"fb": {
+		"appId": false,
+		"appSecret": false,
+		"callbackURL": ""
+	}
+};
--- webapp/index.js
+++ webapp/index.js
@@ -1,16 +1,26 @@
-var express = require('express')
-var app = express();
+'use strict';
+
+const express = require('express');
+const app = express();
+const config = require('./config/config.js');
+const path = require('path');
+
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
-	// test
+	console.log(process.env);
+  response.send('Hello World! Hey girl, let\'s fingerbang!')
 })
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 	console.log("FINGER: " + process.env.FINGER);
+	console.log("config: " + config.appName);
+	console.log("config: " + config.appVersion);
+	console.log("config: " + config.sessionSecret);
+
+
 })
 
